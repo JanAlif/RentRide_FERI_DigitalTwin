54eed46c9acf842e7088363894a0ebab64c34434
Tit Podhraški <143614117+Tit-Hub230@users.noreply.github.com>
2024-05-23 11:48:26 +0200

    Implementacija google/car api-jev

--- Principi/untitled/build.gradle.kts
+++ Principi/untitled/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
 
 tasks.test {
