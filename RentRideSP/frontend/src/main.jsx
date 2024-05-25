981b5bea2ca885809598be82ec6340c8897b92ca
fool1sh <anej.mumel@icloud.com>
2024-05-25 10:16:58 +0200

    KAN-22 frontend layout

--- RentRideSP/frontend/src/main.jsx
+++ RentRideSP/frontend/src/main.jsx
@@ -3,13 +3,40 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 //this is for Material Tailwind
 import { ThemeProvider } from "@material-tailwind/react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import HeroScreen from "./screens/HeroScreen";
+import { LoginScreen } from "./screens/LoginScreen.jsx";
+
+//router for navigation
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HeroScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      {/* <Route path="/register" element={<RegisterScreen />} /> */}
+      <Route path="" element={<PrivateRoute />}></Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </React.StrictMode>
+  </Provider>
 );
--- RentRideSP/frontend/vite.config.js
+++ RentRideSP/frontend/vite.config.js
@@ -4,11 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 4001 },
-  proxy: {
-    "/api": {
-      target: "http://localhost:4000",
-      changeOrigin: true,
+  server: {
+    port: 4001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
     },
   },
 });
