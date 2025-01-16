0ca069953f19b24f793b822cb5702c9ebb35b457
JanAlif <jan112alif@gmail.com>
2025-01-16 15:49:55 +0100

    Added marker for db points

--- RRI_projekt/core/src/main/java/si/um/feri/cestar/assets/RegionNames.java
+++ RRI_projekt/core/src/main/java/si/um/feri/cestar/assets/RegionNames.java
@@ -6,4 +6,15 @@ public class RegionNames {
     public static final String FINISH = "finish";
     public static final String WINDOW = "stop";
     public static final String BACKGROUND = "background";
+    public static final String MARKER = "marker";
+   // public static final String STOP = "stop";
+    public static final String ARROW = "arrow";
+    public static final String NOTURN = "no-u-turn";
+    public static final String PARKING = "parking";
+    public static final String ROADWORK = "roadwork";
+    public static final String TURNRIGHT = "turn-right";
+    public static final String TURNLEFT = "turn-left";
+    public static final String NOENTRY = "no-entry";
+    public static final String STOPSIGN = "stopSign";
+
 }
